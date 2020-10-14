2f6d75752b4045b51a422ad80156da1b7fa8705a
mingray <mingray_hsu@perfectcorp.com>
2020-10-14 16:31:08 +0800

    Updates

---
+++ precache-manifest.9ed4c611c3dcdfe5467012029be206e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cba2f1521ab514262f9f14e8eee619",
+    "url": "/QRcode-App/index.html"
+  },
+  {
+    "revision": "48e18b265427d0eb49ed",
+    "url": "/QRcode-App/static/js/2.e560c4f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QRcode-App/static/js/2.e560c4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1e95e4590029ab5c772",
+    "url": "/QRcode-App/static/js/main.395efe22.chunk.js"
+  },
+  {
+    "revision": "9c9c775e8fa0fe16daad",
+    "url": "/QRcode-App/static/js/runtime-main.ef51c166.js"
+  }
+]);
